670503e93699277b2600e320c2c38e1522a33cfb
ChickenMan4236 <chickenmanpl@protonmail.com>
2021-05-11 00:52:33 +0200

    working ptls

--- src/ptLs.sh
+++ src/ptLs.sh
@@ -9,7 +9,7 @@ function getColumnSize(){
     while read -r line; do
         read -ra words <<< $line
         for val in "${words[@]}"; do
-            if (($columnSize < ${#val})); then
+            if ((columnSize < ${#val})); then
                 columnSize=${#val}
             fi
         done 
@@ -29,6 +29,12 @@ function setArgs(){
     fi
 }
 
+function align(){
+    for (( i=0; i<$((columnSize-(actualChar%columnSize))); i++ )); do
+        echo -n " "
+    done
+}
+
 arg="-"
 setArgs $1
 
@@ -37,74 +43,54 @@ if [[ ! -z $arg ]];
     then a=$arg
 fi
 
-LS=$(ls ${a}l --group-directories-first 2>/dev/null)
+LS=$(ls ${a}l --group-directories-first --time-style=+"" 2>/dev/null)
 
 getColumnSize
 
-columnSize=$(($columnSize+$LS_MIN_FILE_OFFSET))
+columnSize=$((columnSize+LS_MIN_FILE_OFFSET))
 
 if ((${#DIR_PREFIX} > ${#FILE_PREFIX})); then
-    columnSize=$(($columnSize+${#DIR_PREFIX}))
+    columnSize=$((columnSize+${#DIR_PREFIX}))
 else
-    columnSize=$(($columnSize+${#FILE_PREFIX}))
+    columnSize=$((columnSize+${#FILE_PREFIX}))
 fi
 
-columns=$(($(tput cols) / $columnSize))
+columns=$(($(tput cols) / columnSize))
 i=0
 actualChar=0
 actualColumn=0
 
-echo "$LS" | while read -r line; do
-    if ((i > 0)); then
-        read -a words <<< $line
-        word=0
-        wordCount=$(echo $line | wc -w)
-        lastWord=$(($wordCount-1))
-        if [[ ${line} == *"->"* ]]; then
-            lastWord=$((lastWord-2))
-        fi
-        dir=0
-        for val in "${words[@]}"; do
-            if ((word == 0)); then
-                if [ ${val:0:1} == "d" ]; then
-                    dir=1
-                    echo -ne $DIR_PREFIX_ESCAPE_CODES
-                    echo -n "$DIR_PREFIX"
-                    actualChar=$((actualChar+${#DIR_PREFIX}))
-                else
-                    echo -ne $FILE_PREFIX_ESCAPE_CODES
-                    echo -n "$FILE_PREFIX"
-                    actualChar=$((actualChar+${#FILE_PREFIX}))
-                fi
-                echo -ne "\e[0m"
-            fi
-            if ((word == lastWord)); then
-                if (($dir==1)); then
-                    echo -ne $DIR_NAME_ESCAPE_CODES
-                else
-                    echo -ne $FILE_NAME_ESCAPE_CODES
-                fi
-                echo -ne "${val}\e[0m"
-                actualChar=$((actualChar+${#val}))
+while read -r line; do
+    read -a words <<< $line
+    if [[ $(echo $line | wc -w) == 2 ]]; then continue; fi
 
-                if [[ $1 == *"l"* ]]; then
-                    echo ""
-                else
-                    for (( i=0; i<$((columnSize-$((actualChar%columnSize)))); i++ )); do
-                        echo -n " "
-                    done
-                    actualChar=$((actualChar+$((columnSize-$((actualChar%columnSize))))))
-                    actualColumn=$((actualColumn+1))
-                    if (($actualColumn >= $columns)); then
-                        echo
-                        actualColumn=0
-                        actualChar=0
-                    fi
-                fi
-            fi
-            word=$((word+1))
-        done
+    if [[ ${words[0]} == "d"* ]]; then
+        filename="${DIR_PREFIX_ESCAPE_CODES}${DIR_PREFIX}\e[0m${DIR_NAME_ESCAPE_CODES}"
+    elif [[ ${words[0]} == "l"* ]];then
+        filename="${DIR_PREFIX_ESCAPE_CODES}link\e[0m${DIR_NAME_ESCAPE_CODES}"
+    else
+        filename="${FILE_PREFIX_ESCAPE_CODES}${FILE_PREFIX}\e[0m${FILE_NAME_ESCAPE_CODES}"
     fi
-    i=$((i+1))
-done
+
+    filename="$filename${words[5]}\e[0m"
+    
+    if [[ $1 == *"l"* ]]; then
+        echo -ne $filename
+        actualChar=${#filename}
+        align        
+        echo "${words[0]} ${words[2]} ${words[3]}"
+    else
+        echo -ne $filename
+        actualChar=$((actualChar+${#filename}))
+        align
+        actualChar=$((actualChar+(columnSize-(actualChar%columnSize))))
+        actualColumn=$((actualColumn+1))
+        if (($actualColumn >= $columns)); then
+            echo
+            actualColumn=0
+            actualChar=0
+        fi
+    fi
+done <<<$(echo "$LS")
+echo
 echo
