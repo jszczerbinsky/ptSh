190e6f3cc0c58900a373bd73a528913505b059f3
ChickenMan4236 <chickenmanpl@protonmail.com>
2021-05-15 00:12:34 +0200

    block displaying

--- src/ptls/main.c
+++ src/ptls/main.c
@@ -1,4 +1,5 @@
 #include <sys/ioctl.h>
+#include <unistd.h>
 #include <string.h>
 
 #include "ptls.h"
@@ -11,11 +12,47 @@ bool fileVisible(FileInstance *file, Args *args)
     return true;
 }
 
+void displayBlock(FileInstance **files, int count, Args* args, int longestName, int* actualColumn, int* actualChar)
+{
+  if(longestName == 0) return;
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+  int columns = w.ws_col/(longestName+1);
+
+  for(int i = 0; i < count; i++)
+  {
+    printf("%s", files[i]->name);
+    (*actualChar)+=strlen(files[i]->name);
+    int spaces = longestName+1 - ((*actualChar)%(longestName+1));
+    for(int x = 0; x < spaces; x++)
+    {
+      printf(" ");
+      (*actualChar)++;
+    }
+    (*actualColumn)++;
+    if((*actualColumn) >= columns)
+    {
+      printf("\n");
+      (*actualChar) = 0;
+      (*actualColumn) = 0;
+    }
+  }
+}
+
+void displayList(FileInstance **files, int count, Args* args, int longestName)
+{
+
+}
+
+void display(FileInstance **files, int count, Args* args, int longestName, int* actualColumn, int* actualChar)
+{
+  if(args->l) displayList(files, count, args, longestName);
+  else displayBlock(files, count, args, longestName, actualColumn, actualChar);
+}
+
 int main(int argc, char** argv)
 {
-  struct winsize w;
-  ioctl(0, TIOCGWINSZ, &w); 
-
   Args *args = parseArgs(argc, argv);
 
   Files *allFiles = getFiles();
@@ -36,10 +73,20 @@ int main(int argc, char** argv)
 
   int actualDir = 0;
   int actualFile = 0;
+
+  int longestName = 0;
   
   for(int i = 0; i < allFiles->count; i++)
   {
-    if(!fileVisible(allFiles->instances[i], args)) continue;
+    if(!fileVisible(allFiles->instances[i], args)){
+      free(allFiles->instances[i]->stats);
+      free(allFiles->instances[i]->name);
+      free(allFiles->instances[i]);
+      continue;
+    }
+
+    int nameLength = strlen(allFiles->instances[i]->name);
+    if(nameLength > longestName) longestName = nameLength;
 
     if(!args->noDirsTop && S_ISDIR(allFiles->instances[i]->stats->st_mode))    
     {
@@ -55,16 +102,26 @@ int main(int argc, char** argv)
   free(allFiles);
 
   sort(dirs, args, 0, dirCount-1);
-
-  for(int i = 0; i < dirCount; i++)
-    printf("%s\n", dirs[i]->name);
-
   sort(files, args, 0, fileCount-1);
 
+  int actualColumn = 0;
+  int actualChar = 0;
+
+  display(dirs, dirCount, args, longestName, &actualColumn, &actualChar);
+  display(files, fileCount, args, longestName, &actualColumn, &actualChar);
+
+  printf("\n");
+
+  for(int i = 0; i < dirCount; i++)
+  {
+    free(dirs[i]->name);
+    free(dirs[i]->stats);
+  }
   for(int i = 0; i < fileCount; i++)
-    printf("%s\n", files[i]->name);
-
-
+  {
+    free(files[i]->name);
+    free(files[i]->stats);
+  }
   free(dirs);
   free(files);
 
