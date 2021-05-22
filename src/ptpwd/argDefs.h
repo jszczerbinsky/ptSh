16bded244455d639148041617dda2a5536f3395e
ChickenMan4236 <chickenmanpl@protonmail.com>
2021-05-22 14:40:15 +0200

    ptpwd display dirs

--- src/ptpwd/argDefs.h
+++ src/ptpwd/argDefs.h
@@ -1,9 +1,12 @@
 #ifndef PTPWD_ARG_DEFS_H
 #define PTPWD_ARG_DEFS_H
 
-#define LOGICAL_PATH_ARG    'L'
+#define LOGICAL_PATH_ARG       'L'
+#define LOGICAL_PATH_ARG_W     "--logical"
+#define LOGICAL_PATH_ARG_DESC  ""
 
-#define HELP_ARG_W          "--help"
-#define LOGICAL_PATH_ARG_W  "--logical"
+#define HELP_ARG               ' '
+#define HELP_ARG_W             "--help"
+#define HELP_ARG_DESC          "--help"
 
 #endif
--- src/ptpwd/ptpwd.h
+++ src/ptpwd/ptpwd.h
@@ -1,1 +1,9 @@
+#ifndef PTPWD_H
+#define PTPWD_H
+
+#include <stdlib.h>
 #include "types.h"
+
+Args *parseArgs(int argc, char **argv);
+
+#endif
